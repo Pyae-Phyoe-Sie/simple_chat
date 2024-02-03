ba1a7553443903480c59b1b996d368cd6bd2c9a6
June TDS <tds.mm.dev004@gmail.com>
2024-02-03 23:16:08 +0630

    Add basic chat function

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,44 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
 import styles from "./page.module.css";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [gender, setGender] = useState(0);
+
+  const inputHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const selectHandler = (event) => {
+    setGender(event.target.value);
+  };
+
+  function gotoChat() {
+    router.push(`/chat?name=${username}&gender=${gender}`);
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="col-6 my-5 mx-auto">
+        <form className="row g-3">
+          <div className="col-md-6">
+            <label htmlFor="username" className="form-label">Please key in your name</label>
+            <input type="text" className="form-control" id="username" onKeyUp={inputHandler} />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="username" className="form-label">Please key in your name</label>
+            <select className="form-control" onChange={selectHandler}>
+              <option value="0" selected>Male</option>
+              <option value="1">Female</option>
+            </select>
+          </div>
+          <div className="col-12">
+            <button type="button" onClick={gotoChat} className="btn btn-primary">Go to Chat</button>
+          </div>
+        </form>
       </div>
     </main>
   );
